041e0ee361e1cde41c05beafd7eb553e8e81d3b3
pbrayer <skellocrusha@yahoo.com>
2019-03-06 18:35:58 -0600

    added fonts and new buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,33 +28,24 @@ $.ajax({
     url: queryURL,
     method: "GET"
   })
-    // After the data comes back from the API
+    
     .then(function(response) {
-      // Storing an array of results in the results variable
+      
       var results = response.data;
       console.log(results)
 
-      // Looping over every result item
       for (var i = 0; i < results.length; i++) {
 
-          // Only taking action if the photo has an appropriate rating
           if (results[i].rating !== "r") {
-          // Creating a div for the gif
+          
           var gifDiv = $("<div>");
 
-          // Storing the result item's rating
            var rating = results[i].rating;
 
-          // Creating a paragraph tag with the result item's rating
           var p = $("<p>").html("<strong>"+ "Rating: "  + rating);
           p.css("text-align", "center")
           
-
-          // Creating an image tag
           var pokemonImage = $("<img>");
-
-          // Giving the image tag an src attribute of a proprty pulled off the
-          // result item
           
           pokemonImage.attr("src", results[i].images.fixed_height_still.url);
           pokemonImage.attr("data-state", "still");
@@ -65,11 +56,9 @@ $.ajax({
           pokemonImage.attr("height","300px")
           gifDiv.css("float", "left")
 
-          // Appending the paragraph and personImage we created to the "gifDiv" div we created
           gifDiv.append(p);
           gifDiv.append(pokemonImage);
 
-          // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
           $("#pokemons").prepend(gifDiv);
            }
       }
@@ -79,9 +68,7 @@ $.ajax({
     $(document).on("click", ".pokemons", displayPokemon);
     $(document).on("click", ".pokemonpic", function(){
         var state = $(this).attr("data-state");
-        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-        // Then, set the image's data-state to animate
-        // Else set src to the data-still value
+       
         if (state === "still") {
           $(this).attr("src", $(this).attr("data-animate"));
           $(this).attr("data-state", "animate");
